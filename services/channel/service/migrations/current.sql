ab1acae1519af45806ae391f09752e51e6cd3c2e
Parmvir Grewal <parmvir.grewal.s@gmail.com>
2026-04-22 20:38:18 +0100

    Changing message on migration

--- services/channel/service/migrations/current.sql
+++ services/channel/service/migrations/current.sql
@@ -1,4 +1,4 @@
---! Message: adding-external_id-removing-original_title
+--! Message: adding-external_id-tp-programs-table
 
 ALTER TABLE app_public.programs ADD COLUMN IF NOT EXISTS external_id TEXT;
 GRANT INSERT (external_id) ON app_public.programs TO ":DATABASE_GQL_ROLE";
